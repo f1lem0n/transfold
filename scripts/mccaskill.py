7008187e6782713b823b09751c5f6949c5cdec1d
Filip Hajdyla <hajdylaf@gmail.com>
2023-11-18 13:10:09 +0100

    change conda icon in starship theme

--- scripts/mccaskill.py
+++ scripts/mccaskill.py
@@ -51,45 +51,48 @@ def create_scoring_tables(
 ) -> tuple[np.ndarray, np.ndarray]:
     # TODO define equation for iters if possible
     n = len(seq) + 1
-    qbp = np.zeros((n, n))
-    q = np.ones((n, n))
+    q_paired = np.zeros((n, n))
+    q_unpaired = np.ones((n, n))
     for _ in range(iters):
-        for i, j in np.ndindex(q.shape):
+        for i, j in np.ndindex(q_unpaired.shape):
             # 1-based indexing and skipping fields below diagonal
             if j == 0 or i == 0 or i > j:
                 continue
-            qbp[i][j] = (
-                q[i + 1][j - 1] * np.exp(-BP_ENERGY_WEIGHT / NORMALIZED_RT)
+            q_paired[i][j] = (
+                q_unpaired[i + 1][j - 1]
+                * np.exp(-BP_ENERGY_WEIGHT / NORMALIZED_RT)
                 if check_pairing(seq[i - 1], seq[j - 1])
                 else 0
             )
             ks = [k for k in range(i, j) if i <= k < j - MIN_LOOP_LENGTH]
-            q[i][j] = q[i][j - 1] + sum(q[i][k - 1] * qbp[k][j] for k in ks)
-    q.astype(np.float64)
-    qbp.astype(np.float64)
-    return q, qbp
+            q_unpaired[i][j] = q_unpaired[i][j - 1] + sum(
+                q_unpaired[i][k - 1] * q_paired[k][j] for k in ks
+            )
+    q_unpaired.astype(np.float64)
+    q_paired.astype(np.float64)
+    return q_unpaired, q_paired
 
 
 def calc_paired_unpaired_probabilities(
     q: np.ndarray, qbp: np.ndarray, iters
 ) -> tuple[np.ndarray, np.ndarray]:
-    p = np.zeros(q.shape)
-    pbp = np.zeros(qbp.shape)
+    p_paired = np.zeros(qbp.shape)
+    p_unpaired = np.zeros(q.shape)
     for _ in range(iters):
-        for i, j in np.ndindex(p.shape):
+        for i, j in np.ndindex(p_unpaired.shape):
             # 1-based indexing and skipping fields below diagonal
             if j == 0 or i == 0 or i > j:
                 continue
             ks = [k for k in range(i, j) if k < i]  # noqa
             ls = [l for l in range(i, j) if j < l]  # noqa
             try:
-                pbp[i][j] = (
+                p_paired[i][j] = (
                     q[1][i - 1]
                     * qbp[i][j]
                     * q[j + 1][len(q) - 1]
                     / q[1][len(q) - 1]
                 ) + sum(
-                    pbp[k][l]
+                    p_paired[k][l]
                     * (
                         np.exp(-BP_ENERGY_WEIGHT / NORMALIZED_RT)
                         * qbp[k + 1][l - 1]
@@ -99,10 +102,10 @@ def calc_paired_unpaired_probabilities(
                     / qbp[k][l]
                     for k, l in zip(ks, ls)
                 )
-                p[i][j] = (
+                p_unpaired[i][j] = (
                     q[1][i - 1] * q[j + 1][len(q) - 1] / q[1][len(q) - 1]
                 ) + sum(
-                    pbp[k][l]
+                    p_paired[k][l]
                     * (
                         np.exp(-BP_ENERGY_WEIGHT / NORMALIZED_RT)
                         * q[k + 1][i - 1]
@@ -113,7 +116,7 @@ def calc_paired_unpaired_probabilities(
                 )
             except IndexError:
                 pass
-    return p[1:, 1:], pbp[1:, 1:]
+    return p_unpaired[1:, 1:], p_paired[1:, 1:]
 
 
 def main() -> stdout:  # pragma: no cover
@@ -122,10 +125,14 @@ def main() -> stdout:  # pragma: no cover
         print("Invalid sequence!")
         exit(1)
     stdout.print_params(seq)
-    q, qbp = create_scoring_tables(seq, 5)
-    p, pbp = calc_paired_unpaired_probabilities(q, qbp, 5)
-    print(f"P:\n{p.round(2)}\n")
-    print(f"Pbp:\n{pbp.round(2)}\n")
+    q_unpaired, q_paired = create_scoring_tables(seq, 5)
+    print(f"Q:\n{q_unpaired.round(2)}\n")
+    print(f"Qbp:\n{q_paired.round(2)}\n")
+    p_unpaired, p_paired = calc_paired_unpaired_probabilities(
+        q_unpaired, q_paired, 5
+    )
+    print(f"P:\n{p_unpaired.round(2)}\n")
+    print(f"Pbp:\n{p_paired.round(2)}\n")
     return stdout()
 
 
