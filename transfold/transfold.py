d09396eba7a77abf4ae12a81d38c920c95c3e33b
Filip Hajdyla <hajdylaf@gmail.com>
2023-11-30 15:16:51 +0100

    add custom help message for CLI

--- transfold/transfold.py
+++ transfold/transfold.py
@@ -1,13 +1,63 @@
+import io
 from pathlib import Path
 from time import localtime, strftime
 
 import click
 import yaml
+from rich.console import Console
 
-from transfold._version import __version__
-from transfold.modules.downloaders import cds_downloader
-from transfold.modules.logger import get_logger
-from transfold.modules.scope_parser import get_scope_df
+# from transfold._version import __version__
+# from transfold.modules.downloaders import cds_downloader
+# from transfold.modules.logger import get_logger
+# from transfold.modules.scope_parser import get_scope_df
+
+
+ASCII = r"""
+ /$$$$$$$$                                      /$$$$$$          /$$       /$$
+|__  $$__/                                     /$$__  $$        | $$      | $$
+   | $$  /$$$$$$  /$$$$$$  /$$$$$$$   /$$$$$$$| $$  \__//$$$$$$ | $$  /$$$$$$$
+   | $$ /$$__  $$|____  $$| $$__  $$ /$$_____/| $$$$   /$$__  $$| $$ /$$__  $$
+   | $$| $$  \__/ /$$$$$$$| $$  \ $$|  $$$$$$ | $$_/  | $$  \ $$| $$| $$  | $$
+   | $$| $$      /$$__  $$| $$  | $$ \____  $$| $$    | $$  | $$| $$| $$  | $$
+   | $$| $$     |  $$$$$$$| $$  | $$ /$$$$$$$/| $$    |  $$$$$$/| $$|  $$$$$$$
+   |__/|__/      \_______/|__/  |__/|_______/ |__/     \______/ |__/ \_______/
+"""
+
+
+class RichGroup(click.Group):
+    def format_help(self, ctx, formatter):
+        sio = io.StringIO()
+        console = Console(file=sio, force_terminal=True)
+        console.print(f"[bold magenta]{ASCII}[/bold magenta]")
+        console.print(
+            "[bold green]A tool for predicting fold category "
+            "based on transcript sequence and structure.[/bold green]"
+        )
+        # [blue]Commands:[/blue]
+        #     download  Download Transfold Database
+        #     run       Run Transfold
+        #             """
+        #         )
+        console.print(f"\n\n[bold yellow]{self.get_usage(ctx)}[/bold yellow]")
+        console.print(
+            "\n[bold yellow]To display "
+            "help message for a command, run:"
+            "\n\n\ttransfold COMMAND -h[/bold yellow]"
+        )
+        console.print("\n[cyan]Options:[/cyan]")
+        for param in self.get_params(ctx):
+            console.print(
+                f"\t{param.get_help_record(ctx)[0]:<25}"
+                f"{'[ ' + str(param.get_default(ctx)) + ' ]':<15}"
+                f"{param.get_help_record(ctx)[1]:<40}"
+            )
+
+        console.print("\n[blue]Commands:[/blue]")
+        for cmd_name in self.list_commands(ctx):
+            cmd = self.get_command(ctx, cmd_name)
+            console.print(f"\t{cmd_name:<40}{cmd.get_short_help_str(ctx):<40}")
+        console.print("\n")
+        formatter.write(sio.getvalue())
 
 
 def read_yaml_file(filepath):
@@ -19,18 +69,13 @@ def read_yaml_file(filepath):
 
 
 @click.group(
+    cls=RichGroup,
     context_settings=dict(help_option_names=["-h", "--help"]),
     invoke_without_command=True,
     no_args_is_help=True,
 )
 @click.option("--version", "-v", is_flag=True, help="Show version")
 def cli(version):
-    """
-    T R A N S F O L D
-
-    A tool for predicting fold category based on transcript sequence
-    and structure.
-    """
     if version:
         print(f"transfold version: {__version__}")
 
